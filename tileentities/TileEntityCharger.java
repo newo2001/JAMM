b66b7cbbbf056ef89af73c10ca01808a1ba0ae72
newo2001 <JoGamesVideos@gmail.com>
2016-01-05 17:58:33 +0100

    Fixed indenting

--- tileentities/TileEntityCharger.java
+++ tileentities/TileEntityCharger.java
@@ -20,7 +20,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class TileEntityCharger extends TileEntity implements IInventory, IEnergyReceiver {
 	private ItemStack[] inventory;
-    private String customName;
+	private String customName;
 	private int energyStored;
 	private int progress;
 	private boolean isActive;
@@ -30,12 +30,12 @@ public class TileEntityCharger extends TileEntity implements IInventory, IEnergy
 	}
 	
 	public String getCustomName() {
-        return customName;
-    }
+		return customName;
+    	}
 	
 	public void setCustomName(String customName) {
-        customName = this.customName;
-    }
+		customName = this.customName;
+    	}
     
     @Override
 	public int getSizeInventory() {
@@ -45,57 +45,56 @@ public class TileEntityCharger extends TileEntity implements IInventory, IEnergy
 	@Override
 	public ItemStack getStackInSlot(int index) {
 		if (index < 0 || index >= getSizeInventory())
-	        return null;
-	    return inventory[index];
+		return null;
+		return inventory[index];
 	}
 
 	@Override
 	public ItemStack decrStackSize(int index, int count) {
 		if (getStackInSlot(index) != null) {
-	        ItemStack itemstack;
-
-	        if (getStackInSlot(index).stackSize <= count) {
-	            itemstack = this.getStackInSlot(index);
-	            setInventorySlotContents(index, null);
-	            markDirty();
-	            return itemstack;
-	        } else {
-	            itemstack = this.getStackInSlot(index).splitStack(count);
-
-	            if (this.getStackInSlot(index).stackSize <= 0) {
-	                this.setInventorySlotContents(index, null);
-	            } else {
-	                this.setInventorySlotContents(index, getStackInSlot(index));
-	            }
-
-	            markDirty();
-	            return itemstack;
-	        }
-	    } else {
-	        return null;
-	    }
-	}
+			ItemStack itemstack;
+			
+		if (getStackInSlot(index).stackSize <= count) {
+			itemstack = this.getStackInSlot(index);
+			setInventorySlotContents(index, null);
+			markDirty();
+			return itemstack;
+		} else {
+			itemstack = this.getStackInSlot(index).splitStack(count);
+			
+			if (this.getStackInSlot(index).stackSize <= 0) {
+				this.setInventorySlotContents(index, null);
+			} else {
+				this.setInventorySlotContents(index, getStackInSlot(index));
+			}
+			
+			markDirty();
+			return itemstack;
+		}	
+	} else
+		return null;
+}
 
 	@Override
 	public ItemStack getStackInSlotOnClosing(int index) {
 		ItemStack stack = getStackInSlot(index);
-	    setInventorySlotContents(index, null);
-	    return stack;
+		setInventorySlotContents(index, null);
+		return stack;
 	}
 
 	@Override
 	public void setInventorySlotContents(int index, ItemStack stack) {
 		if (index < 0 || index >= this.getSizeInventory())
-	        return;
-
-	    if (stack != null && stack.stackSize > getInventoryStackLimit())
-	        stack.stackSize = this.getInventoryStackLimit();
-	        
-	    if (stack != null && stack.stackSize == 0)
-	        stack = null;
-
-	    inventory[index] = stack;
-	    markDirty();
+			return;
+			
+		if (stack != null && stack.stackSize > getInventoryStackLimit())
+			stack.stackSize = this.getInventoryStackLimit();
+		
+		if (stack != null && stack.stackSize == 0)
+		stack = null;
+		
+		inventory[index] = stack;
+		markDirty();
 	}
 
 	@Override
@@ -120,10 +119,12 @@ public class TileEntityCharger extends TileEntity implements IInventory, IEnergy
 
 	@Override
 	public void openInventory() {
+		
 	}
 
 	@Override
 	public void closeInventory() {
+		
 	}
 
 	@Override
@@ -133,42 +134,41 @@ public class TileEntityCharger extends TileEntity implements IInventory, IEnergy
 	
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
-	    super.writeToNBT(nbt);
-
-	    NBTTagList list = new NBTTagList();
-	    if (getStackInSlot(0) != null) {
-	    	NBTTagCompound stackTag = new NBTTagCompound();
-	        stackTag.setByte("Slot", (byte) 0);
-	        getStackInSlot(0).writeToNBT(stackTag);
-	        list.appendTag(stackTag);
-	    }
-	    nbt.setTag("Items", list);
-
-	    if (this.hasCustomInventoryName()) {
-	        nbt.setString("CustomName", getCustomName());
-	    }
-	    
-	    nbt.setInteger("progress", progress);
-	    nbt.setInteger("energyStored", energyStored);
+		super.writeToNBT(nbt);
+		NBTTagList list = new NBTTagList();
+		if (getStackInSlot(0) != null) {
+			NBTTagCompound stackTag = new NBTTagCompound();
+			stackTag.setByte("Slot", (byte) 0);
+			getStackInSlot(0).writeToNBT(stackTag);
+			list.appendTag(stackTag);
+			
+		}
+		
+		nbt.setTag("Items", list);
+		if (this.hasCustomInventoryName())
+			nbt.setString("CustomName", getCustomName());
+			
+		nbt.setInteger("progress", progress);
+		nbt.setInteger("energyStored", energyStored);
 	}
 
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
-	    super.readFromNBT(nbt);
-
-	    NBTTagList list = nbt.getTagList("Items", 10);
-	    for (int i = 0; i < list.tagCount(); ++i) {
-	        NBTTagCompound stackTag = list.getCompoundTagAt(i);
-	        int slot = stackTag.getByte("Slot") & 255;
-	        setInventorySlotContents(slot, ItemStack.loadItemStackFromNBT(stackTag));
-	    }
-
-	    if (nbt.hasKey("CustomName", 8))
-	        setCustomName(nbt.getString("CustomName"));
-	    
-	    progress = nbt.getInteger("progress");
-	    energyStored = nbt.getInteger("energyStored");
+		super.readFromNBT(nbt);
+		NBTTagList list = nbt.getTagList("Items", 10);
+		
+		for (int i = 0; i < list.tagCount(); ++i) {
+			NBTTagCompound stackTag = list.getCompoundTagAt(i);
+			int slot = stackTag.getByte("Slot") & 255;
+			setInventorySlotContents(slot, ItemStack.loadItemStackFromNBT(stackTag));
+		}
+		
+		if (nbt.hasKey("CustomName", 8))
+			setCustomName(nbt.getString("CustomName"));
+		
+		progress = nbt.getInteger("progress");
+		energyStored = nbt.getInteger("energyStored");
 	}
 	
 	@Override
@@ -227,14 +227,14 @@ public class TileEntityCharger extends TileEntity implements IInventory, IEnergy
 	
 	@Override
 	public Packet getDescriptionPacket() {
-	    NBTTagCompound tag = new NBTTagCompound();
-	    writeToNBT(tag);
-	    return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 0, tag);
+		NBTTagCompound tag = new NBTTagCompound();
+		writeToNBT(tag);
+		return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 0, tag);
 	}
 	
 	@Override
 	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
-	    readFromNBT(pkt.func_148857_g());
+		readFromNBT(pkt.func_148857_g());
 	}
 	
 	public void markForUpdate() {
