6dc8723b6fb7ab8f6df522075c5238a51985f3e6
newo2001 <JoGamesVideos@gmail.com>
2016-01-12 09:45:24 +0100

    Create UmoniaNugget.java

---
+++ items/UmoniaNugget.java
@@ -0,0 +1,11 @@
+packaxe newo2001.JAMM.items;
+
+import net.minecraft.items.Item
+
+public class UmoniaNugget extends Item {
+	public UmoniaNugget() {
+		maxStackSize = 64;
+		setTextureName("JAMM:itemNuggetUmonia");
+		setUnlocalizedName("itemNuggetUmonia");
+	}
+}
