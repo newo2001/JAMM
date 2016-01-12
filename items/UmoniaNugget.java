f2894cef5c4b337dc2a9e75d57ab3281a6c1ee72
newo2001 <JoGamesVideos@gmail.com>
2016-01-12 09:46:39 +0100

    Update UmoniaNugget.java

--- items/UmoniaNugget.java
+++ items/UmoniaNugget.java
@@ -1,11 +1,13 @@
 packaxe newo2001.JAMM.items;
 
 import net.minecraft.items.Item
+import net.minecraft.creativetabs.creativetabs;
 
 public class UmoniaNugget extends Item {
 	public UmoniaNugget() {
 		maxStackSize = 64;
 		setTextureName("JAMM:itemNuggetUmonia");
 		setUnlocalizedName("itemNuggetUmonia");
+		setCreativeTab(CreativeTabs.tabMaterials);
 	}
 }
